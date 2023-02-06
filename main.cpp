2e1c7d3fd73a7e3b79530bc680f83476e62cbe6c
Jouni Sunnari <dz4tune@gmail.com>
2023-02-06 22:28:18 +0100

    moved code to different file

--- main.cpp
+++ main.cpp
@@ -1,44 +1,14 @@
-#include <boost/iterator/transform_iterator.hpp>
-#include <boost/process.hpp>
-#include <chrono>
-#include <cstddef>
-#include <fmt/format.h>
-#include <iostream>
-#include <string>
 #include <thread>
-
-namespace bp = boost::process;
-
-class tqdm_proc {
-public:
-  tqdm_proc(std::size_t total)
-      : _in(),
-        _child(fmt::format("tqdm --null --total {}", total), bp::std_in < _in) {
-
-  }
-
-  void tick() { _in << std::endl; }
-
-  ~tqdm_proc() {
-    _in.pipe().close();
-    _child.wait();
-  }
-
-private:
-  bp::opstream _in;
-  bp::child _child;
-};
+#include "tqdm_proc.hpp"
 
 void baz() {
   tqdm_proc foo(100);
   for (int i = 0; i < 100; ++i) {
-
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
     foo.tick();
   }
 }
 
 int main() {
-  bp::opstream in;
   baz();
 }
